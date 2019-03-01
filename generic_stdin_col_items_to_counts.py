8ea95c4b5dda31703d6776b21c5cb69d4108a4d7
Travis Saari <tsaari987@gmail.com>
2019-03-01 13:24:27 -0500

    cleanup

--- generic_stdin_col_items_to_counts.py
+++ generic_stdin_col_items_to_counts.py
@@ -8,18 +8,16 @@ counts = defaultdict(int)
 if len(sys.argv) < 2:
 	sys.exit('Usage: %s INT.\n   Where INT is index of column number to count (0-based).' % sys.argv[0])
 
+#Which column
 indx = int(sys.argv[1])
 
+#Gather counts
 for line in sys.stdin:
 	linedata = line.split("\t")
-	
 	item = linedata[indx]
 	counts[item] += 1
 
-#Convert to dict - avoid defaultdict's strange behavior when converting
-#from int to string (prints to console during conversion, not sure why)
-countsdict = dict(counts)
-
-for key, value in countsdict.items():
+#Print counts to stdout
+for key, value in counts.items():
 	count = str(value)
 	print(key + '\t' + count)
